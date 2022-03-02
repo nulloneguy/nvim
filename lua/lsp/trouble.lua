0164a26c6e54fedd98e06b7353b37a8ef96adbf7
nulloneguy <nulloneguy@ya.ru>
2022-03-02 19:38:45 +0700

    update

--- lua/lsp/trouble.lua
+++
@@ -1,9 +0,0 @@
-local ok, trouble = pcall(require, "trouble")
-
-if not ok then
-    return
-end
-
-trouble.setup {
-    use_diagnostic_signs = true,
-}
--- lua/plugins/configs/surround.lua
+++
@@ -1,7 +0,0 @@
-local ok, surround = pcall(require, "surround")
-
-if not ok then
-    return
-end
-
-surround.setup { mappings_style = "surround" }
